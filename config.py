cfc2b65b9788ba0b50a5ee25c7867f58108a9622
pd007 <31837194+pd007@users.noreply.github.com>
2018-09-28 11:44:10 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "dtKbfKh0pkUgjedMAY31zaSrK"
+consumer_secret = "XdyyBI7IzTpMs8wCmd1iAu9VFIyZ4c7Srss8BT5p3XBHsXGufp"
+access_token = "1038532858292330496-VIZEG8KX1OyyJMc7etIl5bxbxyxsEW"
+access_token_secret = "kBIPwdFwxM81mztXURtYxKt3NvmLWjGm0NUh1OdkvYE3y"
